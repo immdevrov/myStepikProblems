4bc3d0456f784537f8e5703dd1ad707a962fcce9
Роман Вайсер <immdevrov@gmail.com>
2020-05-08 20:23:55 +0300

    working progress

--- net_work.py
+++ net_work.py
@@ -1,51 +1,64 @@
 class Queue_item:
-  def __init__(self, item, next, prev):
-    self.value = item
-    self.next = next
-    self.prev = prev
+    def __init__(self, item, next, prev):
+        self.value = item
+        self.next = next
+        self.prev = prev
+
 
 class Queue:
-  def __init__(self):
-    self.length = 0
-    self.last = None
-    self.head = None
+    def __init__(self):
+        self.length = 0
+        self.last = None
+        self.head = None
 
-  def add(self, value):
-    self.length += 1
-    if self.length == 1:
-      item = Queue_item(item=value, next=None, prev=None)
-      self.head = item
-      self.last = item
-    else:
-      last = self.last
-      new_last = Queue_item(item=value, next=last, prev=None)
-      self.last.prev = new_last
-      self.last = new_last
-      self.last.prev = None
+    def add(self, value):
+        self.length += 1
+        if self.length == 1:
+            item = Queue_item(item=value, next=None, prev=None)
+            self.head = item
+            self.last = item
+        else:
+            last = self.last
+            new_last = Queue_item(item=value, next=last, prev=None)
+            self.last.prev = new_last
+            self.last = new_last
+            self.last.prev = None
 
-  def pop(self):
-    if self.length == 1:
-      self.length = 0
-      return self.head.value
-    else:
-      head = self.head
-      self.head = self.head.prev
-      self.head.next = None
-      self.length -= 1
-      return head.value
+    def pop(self):
+        if self.length == 1:
+            self.length = 0
+            return self.head.value
+        else:
+            head = self.head
+            self.head = self.head.prev
+            self.head.next = None
+            self.length -= 1
+            return head.value
 
-# [size, n] = map(int, input().split(' '))
-# arrival = []
-# duration = []
-# for i in range(n):
-#   [a, d] = map(int, input().split(' '))
-#   arrival.append(a)
-#   duration.append(d)
+
+[size, n] = map(int, input().split(' '))
+arrival = []
+duration = []
+handle_time = []
+for i in range(n):
+    [a, d] = map(int, input().split(' '))
+    arrival.append(a)
+    duration.append(d)
+    handle_time.append(-1)
 
 queue = Queue()
-queue.add('7')
-queue.add('8')
-queue.add('9')
-print(queue.pop())
-print(queue.pop())
-print(queue.pop())
+
+
+free_time = 0
+time_of_first = 0
+
+for i in range(n):
+    if arrival[i] >= free_time:
+        if queue.length < size:
+            queue.add(i)
+            handle_time[i] = max(arrival[i], time_of_first)
+            time_of_first = handle_time[i] + duration[i]
+        else:
+            queue.pop()
+
+print(handle_time)
